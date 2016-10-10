279d560fd85530a9788bb30ffb58d45d74700b11
Feilin Wu <feilinwu2036@gmail.com>
2016-10-10 16:06:33 +0800

    init commit

---
+++ src/main/java/com/example/imooc/weixin/entity/Message.java
@@ -0,0 +1,40 @@
+package com.example.imooc.weixin.entity;
+
+/**
+ * 
+ * 消息实体
+ */
+public class Message {
+
+	private String 	id; 		//主键
+	private String 	command;
+	private String 	description;
+	private String 	content;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getCommand() {
+		return command;
+	}
+	public void setCommand(String command) {
+		this.command = command;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+}
---
+++ src/main/java/com/example/imooc/weixin/servlet/ListServlet.java
@@ -0,0 +1,52 @@
+package com.example.imooc.weixin.servlet;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ListServlet
+ */
+public class ListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public ListServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection connection = DriverManager.getConnection("jdbc:mysql//localhost:3306/imooc", 
+					"root", "root");
+			String sql = "";
+			PreparedStatement statement = connection.prepareStatement(sql);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		request.getRequestDispatcher("/WEB-INF/jsp/backend/data.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
